7ac66511dd193ef1fc384f53925414e5d334d223
baby <yshi914@outlook.com>
2020-10-06 11:57:33 +1300

    implement App component

--- album-viewer-app/src/App.js
+++ album-viewer-app/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Album from "./pages/Album";
+import Photo from "./pages/Photo";
+import FullPhoto from "./pages/FullPhoto";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Album} />
+          <Route exact path="/photo/:id" component={Photo} />
+          <Route path="/photo/full/:id" component={FullPhoto} />
+        </Switch>
+      </Router>
     </div>
   );
 }
